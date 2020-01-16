955b6683fdaf64ce905c553f2383892b0800f5a5
Will Axtell <waxtell@transtar1.com>
2020-01-16 09:44:29 -0500

    Added XML documentation and release notes.

--- Swashbuckle.AWSApiGateway.Annotations/Options/XAmazonApiGatewayOptions.cs
+++ Swashbuckle.AWSApiGateway.Annotations/Options/XAmazonApiGatewayOptions.cs
@@ -11,11 +11,19 @@ namespace Swashbuckle.AWSApiGateway.Annotations
         private readonly XAmazonApiGatewayKeySourceOptions _apiKeySourceOptions = new XAmazonApiGatewayKeySourceOptions();
         private readonly XAmazonApiGatewayCORSOptions _corsOptions = new XAmazonApiGatewayCORSOptions();
 
+        /// <summary>
+        /// Specify the source to receive an API key to throttle API methods that require a key.
+        /// </summary>
+        /// <param name="setupAction"></param>
         public void WithKeySource(Action<XAmazonApiGatewayKeySourceOptions> setupAction)
         {
             setupAction.Invoke(_apiKeySourceOptions);
         }
 
+        /// <summary>
+        /// Specifies the cross-origin resource sharing (CORS) configuration for an HTTP API. The extension applies to the root-level OpenAPI structure.
+        /// </summary>
+        /// <param name="setupAction"></param>
         public void WithCors(Action<XAmazonApiGatewayCORSOptions> setupAction)
         {
             setupAction.Invoke(_corsOptions);
